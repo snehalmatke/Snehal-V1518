753ee587365e124703b5067e6d9d7fd0eb37736a
VPEL <VPEL@DESKTOP-LB4RHOS>
2023-01-22 20:38:31 +0530

    snehal 2212023 V1518

---
+++ Eureka_Service_Registry/src/test/java/com/vst/Eureka_Service_Registry/EurekaServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vst.Eureka_Service_Registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
